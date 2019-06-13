3aee66ca561742d723a94b9fe5130bab618d0094
Adam McDevitt <amcdevitt@scottlogic.com>
2019-06-13 09:51:32 +0100

    removed duplicate files

--- tests/getPupilDisplacement.test.js
+++ tests/getPupilDisplacement.test.js
@@ -1,11 +1,32 @@
 const getPupilDisplacement = require('../src/static/js/gui').getPupilDisplacement;
-test('basic', () => {
+test('eye looks to top right', () => {
     var coords = [-1, 1];
-    var max_displacement = 1000;
-    var x_fov_bound = 1;
-    var y_fov_bound = 1;
-    var pupilDisplacement = getPupilDisplacement(coords, max_displacement, x_fov_bound, y_fov_bound);
+    var maxDisplacement = 1000;
+    var xFovBound = 1;
+    var yFovBound = 1;
+    var pupilDisplacement = getPupilDisplacement(coords, maxDisplacement, xFovBound, yFovBound);
     expect(pupilDisplacement[0]).toBeCloseTo(Math.sqrt(1000 * 1000 / 2));
     expect(pupilDisplacement[1]).toBeCloseTo(Math.sqrt(1000 * 1000 / 2));
 });
+
+test('eye looks straight ahead', () => {
+    var coords = [0,0];
+    var maxDisplacement = 1000;
+    var xFovBound = 1;
+    var yFovBound = 1;
+    var pupilDisplacement = getPupilDisplacement(coords, maxDisplacement, xFovBound, yFovBound);
+    expect(pupilDisplacement[0]).toBeCloseTo(0);
+    expect(pupilDisplacement[1]).toBeCloseTo(0);
+});
+
+test('small x fov bound', () => {
+    var coords = [0,0];
+    var maxDisplacement = 1000;
+    var xFovBound = 1;
+    var yFovBound = 1;
+    var pupilDisplacement = getPupilDisplacement(coords, maxDisplacement, xFovBound, yFovBound);
+    expect(pupilDisplacement[0]).toBeCloseTo(0);
+    expect(pupilDisplacement[1]).toBeCloseTo(0);
+});
+
 
