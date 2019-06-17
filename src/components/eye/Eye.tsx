0645d6ef9cf4f71fb41ce9a72cc2d876f7813fe8
Jerome Illgner <jillgner@scottlogic.com>
2019-06-17 17:00:59 +0100

    Refactor/12 eye graphics (#40)
    * created react app with typescript and added Eye component
    
    * removed the deployment job from the workflow in the circleci config
    
    * clean up
    
    * updated dependencies - moved jest and types into the devDependencies
    
    * created const function to pass a window as a prop to the app, removed the width and height from the state in the Eye component
    
    * clean up - removed unused logo, removed input element from app.tsx
    
    * eyeball rendering no longer uses d3
    
    * fixed windows/environment discrepancy
    
    * resolved window/environment discrepancies

--- src/components/eye/Eye.tsx
+++ src/components/eye/Eye.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { select } from 'd3-selection';
 
 interface IEyeProps {
     class: string,
@@ -7,75 +6,47 @@ interface IEyeProps {
     height: number,
     scleraColor: string,
     irisColor: string,
-    pupilColor: string,
+    pupilColor: string
 }
 
 interface IEyeState {
-    x_pos: number,
-    y_pos: number,
     scleraSize: number,
     irisSize: number,
     pupilSize: number,
 }
 
 export default class Eye extends React.Component<IEyeProps, IEyeState> {
-    svg: any
-    // svg!: Selection<SVGGElement | null, {}, HTMLElement, undefined> | Selection<SVGGElement | null, {}, null, undefined>;
     constructor(props: IEyeProps) {
         super(props);
 
         this.state = {
-            // x and y pos to be used in moving the eyes
-            x_pos: 0,
-            y_pos: 0,
             scleraSize: 4,
             irisSize: 8,
             pupilSize: 16,
         }
     }
 
-    componentDidUpdate() {
-        this.renderEye();
-    }
-
-    componentDidMount() {
-        this.renderEye();
-    }
-
-    renderEye() {
-        this.svg.selectAll("*").remove();
-        // set the svg size
-        let svg = this.svg
-            .attr("width", this.props.width)
-            .attr("height", this.props.height)
-            .append("g")
-            .attr("transform", "translate(" + (this.props.width / 2) + "," + (this.props.height / 2) + ")")
-
-        // create sclera
-        svg.append("circle")
-            .attr("class", this.props.class + "Sclera")
-            .attr("r", this.props.width / this.state.scleraSize)
-            .style("fill", this.props.scleraColor)
-        var leftInner = svg.append("g").attr("class", "leftInner");
-        // create iris
-        leftInner.append("circle")
-            .attr("class", this.props.class + "Iris")
-            .attr("r", this.props.width / this.state.irisSize)
-            .style("fill", this.props.irisColor)
-        // create pupil
-        leftInner.append("circle")
-            .attr("class", this.props.class + "Pupil")
-            .attr("r", this.props.width / this.state.pupilSize)
-            .style("fill", this.props.pupilColor)
-        this.svg = svg;
+    renderCircle(radius : number, name : string, colour: string){
+        return(
+        <circle 
+            r={radius}
+            className={name}
+            fill={colour}
+            cx="50%"
+            cy="50%"
+        />
+        )
     }
 
     render() {
         return (
-            <svg
-                className={this.props.class}
-                ref={element => this.svg = select(element)}
-            />
-        )
+            <svg className={this.props.class} width={this.props.width} height={this.props.height}>
+                {this.renderCircle(this.props.width / this.state.scleraSize, "sclera", this.props.scleraColor)}
+                <g className="inner">
+                    {this.renderCircle(this.props.width / this.state.irisSize, "iris", this.props.irisColor)}
+                    {this.renderCircle(this.props.width / this.state.pupilSize, "pupil", this.props.pupilColor)}
+                </g>
+            </svg>
+            )
     }
 }
