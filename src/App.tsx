b94931223ef2a2a067966694c4cbc0d6e9cccb06
Adam McDevitt <amcdevitt@scottlogic.com>
2019-06-17 13:19:36 +0100

    made example function for ConfigMenu

--- src/App.tsx
+++ src/App.tsx
@@ -50,6 +50,7 @@ class App extends React.Component<{}, IAppState> {
   }
 
   render() {
+    return example();
     return (
       <div className="App">
         {Object.values(eyes).map((eye, key) => {
