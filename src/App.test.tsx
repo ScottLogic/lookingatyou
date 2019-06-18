d345d0803b5f3e9244fa08066e99e3bb18398f82
Marcin Szczot <mszczot@scottlogic.com>
2019-06-18 11:49:50 +0100

    fixed the default test by passing a mock window prop

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import jsdom from 'jsdom';
 import App from './App';
 
 it('renders without crashing', () => {
+  const window = new jsdom.JSDOM().window;
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App environment={window} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
