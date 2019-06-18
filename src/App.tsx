f84ddd9df91945e1e2d827e5510305da8fafcc29
marcinscottlogic <51320716+marcinscottlogic@users.noreply.github.com>
2019-06-18 14:13:11 +0100

    Refactor/37 webcam (#42)
    * created webcamFeed component
    
    * added WebcamFeed to app component
    
    * fixed the default test by passing a mock window prop
    
    * removed console.log and return statements, and updated the app and webcamFeed component with consistent semicolons
    
    * added semicolons in app
    
    * removed trailing > from the example component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Eye from './components/eye/Eye';
 import { TextBoxMenuItem, CheckBoxMenuItem } from './components/ConfigMenu/MenuItem';
 import { ConfigMenu, Example } from './components/ConfigMenu/ConfigMenu';
 import './App.css';
+import WebcamFeed from './components/webcamFeed/WebcamFeed';
 
 const eyes = {
   LEFT: 'left',
@@ -15,9 +16,13 @@ const colours = {
   pupilColor: "black"
 }
 
+const videoinput = 'videoinput';
+
 interface IAppState {
   width: number,
   height: number,
+  eyesDisplayed: boolean,
+  webcams: MediaDeviceInfo[],
 }
 
 interface IAppProps {
@@ -31,17 +36,30 @@ class App extends React.Component<IAppProps, IAppState> {
     this.state = {
       width: this.props.environment.innerWidth,
       height: this.props.environment.innerHeight,
+      eyesDisplayed: false,
+      webcams: [],
     }
 
     this.updateDimensions = this.updateDimensions.bind(this);
+    this.onUserMedia = this.onUserMedia.bind(this);
+    this.onUserMediaError = this.onUserMediaError.bind(this);
   }
 
   componentDidMount() {
     this.props.environment.addEventListener("resize", this.updateDimensions);
+    this.getWebcamDevices();
   }
 
   componentWillUnmount() {
     this.props.environment.removeEventListener("resize", this.updateDimensions);
+  }
+
+  async getWebcamDevices() {
+    let devices = await navigator.mediaDevices.enumerateDevices();
+    devices = devices.filter(device => device.kind === videoinput);
+    this.setState({
+      webcams: devices
+    });
   }
 
   updateDimensions() {
@@ -51,21 +69,43 @@ class App extends React.Component<IAppProps, IAppState> {
     });
   }
 
+  onUserMedia(stream: MediaStream) {
+    this.setState({ eyesDisplayed: true });
+  }
+
+  onUserMediaError() {
+    this.setState({ eyesDisplayed: false });
+  }
+
   render() {
     return (
       <div className="App">
-        {Object.values(eyes).map((eye, key) => {
-          return (
-            <Eye
-              class={eye}
-              key={key}
-              width={this.state.width / 2}
-              height={this.state.height}
-              {...colours}
-            />
-          )
-        })}
-        <Example/>>
+        <div className="webcam-feed">
+          {this.state.webcams.map((device, key) => {
+            return (
+              <WebcamFeed
+                key={key}
+                deviceId={device.deviceId}
+                onUserMedia={this.onUserMedia}
+                onUserMediaError={this.onUserMediaError}
+              />
+            )
+          })}
+        </div>
+        <div className={this.state.eyesDisplayed ? 'container' : 'hidden'}>
+          {Object.values(eyes).map((eye, key) => {
+            return (
+              <Eye
+                class={eye}
+                key={key}
+                width={this.state.width / 2}
+                height={this.state.height}
+                {...colours}
+              />
+            )
+          })}
+          <Example/>
+        </div>
       </div>
     );
   }
