2c5ee40fa53fcb9070d87ef96aaa6795c2b3cc30
Adam McDevitt <amcdevitt@scottlogic.com>
2019-06-18 11:15:00 +0100

    changed ConfigMenu to function

--- src/components/ConfigMenu/ConfigMenu.tsx
+++ src/components/ConfigMenu/ConfigMenu.tsx
@@ -1,54 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './ConfigMenu.css';
 import { TextBoxMenuItem, CheckBoxMenuItem } from './MenuItem'
 
 interface IConfigMenuProps {
     width: string,
-    timerLength: number
+    timerLength: number,
+    children: React.ReactNode
 }
 interface IConfigMenuState {
     leftPosition: string
 }
-export class ConfigMenu extends React.Component<IConfigMenuProps, IConfigMenuState> {
-    private isUnderMouse: boolean = false;
-    private timeout: number = window.setTimeout(() => { }, 0);
-    constructor(props: IConfigMenuProps) {
-        super(props);
-        this.state = { leftPosition: "0px" };
-        this.show = this.show.bind(this);
-        this.hide = this.hide.bind(this);
-    }
-
-    componentDidMount() {
-        window.addEventListener("mousemove", this.show);
-    }
-
-    componentDidUnmount() {
-        window.removeEventListener("mousemove", this.show);
-    }
-    
-    show() {
-        clearInterval(this.timeout);
-        if (!this.isUnderMouse)
-            this.timeout = window.setTimeout(this.hide, this.props.timerLength);
-        this.setState({ leftPosition: "0px" });
-    }
-    hide() {
-        this.setState({ leftPosition: "-" + this.props.width });
-    }
-
-    render() {
-        return (
-            <div
-                style={{ width: this.props.width, left: this.state.leftPosition }}
-                className={"ConfigMenu"}
-                onMouseMove={() => { this.isUnderMouse = true }}
-                onMouseLeave={() => { this.isUnderMouse = false }}
-            > <h1>Config</h1>
-                {this.props.children}
-            </div>
-        );
-    }
+export function ConfigMenu(props: IConfigMenuProps) {
+    const [leftPosition, setLeftPosition] = useState("0px");
+    const [isUnderMouse, setIsUnderMouse] = useState(false);
+    const [timeout, setTimeout] = useState(window.setTimeout(() => { }, 0));
+    useEffect(() => {
+        function show() {
+            clearInterval(timeout);
+            if (!isUnderMouse)
+                setTimeout(window.setTimeout(() => setLeftPosition("-" + props.width), props.timerLength));
+            setLeftPosition("0px");
+        }
+        window.addEventListener("mousemove", show);
+        return (() => window.removeEventListener("mousemove", show));
+    })
+    return (
+        <div
+            style={{ width: props.width, left: leftPosition }}
+            className={"ConfigMenu"}
+            onMouseMove={() => { setIsUnderMouse(true) }}
+            onMouseLeave={() => { setIsUnderMouse(false) }}
+        > <h1>Config</h1>
+            {props.children}
+        </div>
+    );
 }
 export function Example() {
     return (
