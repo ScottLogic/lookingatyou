ea3a9fde32c731c3049026d6369267ac99bc07d2
Marcin Szczot <mszczot@scottlogic.com>
2019-06-19 17:07:58 +0100

    created mock mediaDevices object

--- src/components/webcamFeed/WebcamFeed.test.tsx
+++ src/components/webcamFeed/WebcamFeed.test.tsx
@@ -6,35 +6,50 @@ import jsdom from 'jsdom';
 describe('WebcamFeed', () => {
   const streamSuccess = 'Stream received';
   const streamFailure = 'Unable to get stream';
-  const deviceId = '123456';
+  const myDeviceId = '123456';
 
   const mockOnUserMedia = jest.fn(() => streamSuccess);
   const mockOnUserMediaError = jest.fn().mockReturnValue(streamFailure);
   const mockMediaDevices = new jsdom.JSDOM().window.navigator.mediaDevices;
-  const myMediaDevices = jest.fn().mockImplementation(() => {
-  });
-
+  const myMediaDevices: MediaDevices = {
+    ondevicechange: () => { },
+    dispatchEvent: (event: Event): boolean => { return false },
+    addEventListener: () => { },
+    removeEventListener: () => { },
+    getSupportedConstraints: (): MediaTrackSupportedConstraints => { return {} },
+    enumerateDevices: (): Promise<MediaDeviceInfo[]> => { return new Promise(() => []) },
+    getUserMedia: (constraints?: MediaStreamConstraints | undefined): Promise<MediaStream> => {
+      return new Promise((resolve) => {
+        if (constraints && typeof constraints.video === 'object') {
+          const deviceId = constraints.video.deviceId;
+          if (deviceId === myDeviceId) {
+            resolve();
+          }
+        }
+      })
+    },
+  };
+  console.log(myMediaDevices)
 
   const props = {
-    deviceId: deviceId,
+    deviceId: myDeviceId,
     onUserMedia: mockOnUserMedia,
     onUserMediaError: mockOnUserMediaError,
   }
-
 
   it('Should call onUserMediaError when no webcam detected', () => {
     mount(<WebcamFeed mediaDevices={mockMediaDevices} {...props} />);
     expect(mockOnUserMediaError.mock.results[0].value).toBe(streamFailure);
   });
 
-  it('Should call onUserMedia when webcam detected', () => {
-    let myMockMediaDevices = new jsdom.JSDOM().window.navigator.mediaDevices;
-    mount(<WebcamFeed mediaDevices={myMockMediaDevices} {...props} />);
+  it('Should call onUserMedia when webcam detected', async () => {
+    console.log(await myMediaDevices.getUserMedia());
+    mount(<WebcamFeed mediaDevices={myMediaDevices} {...props} />);
     expect(mockOnUserMedia.mock.results[0].value).toBe(streamSuccess);
   })
 
   it('Should render video object', () => {
-    const wrapper = shallow(<WebcamFeed mediaDevices={mockMediaDevices} {...props} />);
+    const wrapper = shallow(<WebcamFeed mediaDevices={mockMediaDevices} {...props} />).debug();
     expect(wrapper).toMatchSnapshot();
   })
 })
