209a12a292be1ed4be1a3402815eee3ad7893d85
Marcin Szczot <mszczot@scottlogic.com>
2019-06-24 10:57:18 +0100

    set the video reducer and integrated it into WebcamFeed component

--- src/components/webcamFeed/WebcamFeed.tsx
+++ src/components/webcamFeed/WebcamFeed.tsx
@@ -1,55 +1,68 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { IVideoState, SET_VIDEO, IDimensions, SET_DIMENSIONS } from '../../store/actions/video/types';
+import { Dispatch } from 'redux';
+import { getDimensions, getVideo } from '../../store/reducers/videoReducer';
 
 interface IWebcamFeedProps {
-    deviceId: string;
-    onUserMedia: (stream: MediaStream) => void;
-    onUserMediaError: (e: Error) => void;
+  deviceId: string,
+  onUserMedia: (stream: MediaStream) => void,
+  onUserMediaError: (e: Error) => void,
+  setDimensions: (dimensions: IDimensions) => void,
+  setVideo: (video: HTMLVideoElement) => void,
+  video: HTMLVideoElement | null,
+  dimensions: IDimensions,
 }
 
-const WebcamFeed = React.forwardRef<HTMLVideoElement, IWebcamFeedProps>(
-    (props, ref) => {
-        const [stream, setStream] = useState();
-        const [width, setWidth] = useState(0);
-        const [height, setHeight] = useState(0);
+const mapStateToProps = (state: IVideoState) => {
+  return {
+    video: getVideo(state),
+    dimensions: getDimensions(state),
+  }
+}
 
-        async function getWebcam() {
-            if (!stream) {
-                try {
-                    const myStream = await getStream(props.deviceId);
-                    const streamSettings = myStream
-                        .getVideoTracks()[0]
-                        .getSettings();
-                    if (streamSettings.height && streamSettings.width) {
-                        setStream(myStream);
-                        setWidth(streamSettings.width);
-                        setHeight(streamSettings.height);
-                    }
-                    props.onUserMedia(myStream);
-                } catch (error) {
-                    props.onUserMediaError(error);
-                }
-            }
+const mapDispatchToProps = (dispatch: Dispatch) => {
+  return {
+    setDimensions: (dimensions: IDimensions) => dispatch({ type: SET_DIMENSIONS, payload: dimensions }),
+    setVideo: (video: HTMLVideoElement) => dispatch({ type: SET_VIDEO, payload: video }),
+  }
+}
+
+function WebcamFeed(props: IWebcamFeedProps) {
+  const [video, setVideo] = useState();
+
+  function getVideo(element: HTMLVideoElement) {
+    if (element) {
+      setVideo(video);
+    }
+  }
+
+  async function getWebcam() {
+    if (!video && !props.video) {
+      try {
+        const myStream = await getStream(props.deviceId);
+        var streamSettings = myStream.getVideoTracks()[0].getSettings();
+        if (streamSettings.height && streamSettings.width) {
+          props.setDimensions({ width: streamSettings.width, height: streamSettings.height })
         }
 
-        useEffect(() => {
-            getWebcam();
-        }, []);
+  function getStream(deviceId: string) {
+    return navigator.mediaDevices.getUserMedia({ video: { deviceId } })
+  }
 
-        function getStream(deviceId: string) {
-            return navigator.mediaDevices.getUserMedia({ video: { deviceId } });
-        }
+  useEffect(() => {
+    getWebcam();
+  })
 
-        return (
-            <video
-                className={props.deviceId + ' hidden'}
-                autoPlay={true}
-                height={height}
-                width={width}
-                src={stream}
-                ref={ref}
-            />
-        );
-    },
-);
+  return (
+    <video
+      className={props.deviceId + ' hidden'}
+      autoPlay={true}
+      height={props.dimensions.height}
+      width={props.dimensions.width}
+      ref={getVideo}
+    />
+  )
+}
 
-export default WebcamFeed;
+export default connect(mapStateToProps, mapDispatchToProps)(WebcamFeed);
