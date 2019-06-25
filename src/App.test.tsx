04cdc752e090277c25aaf0d572102bae92f14a5d
Jerome Illgner <ji8@hw.ac.uk>
2019-06-25 13:00:19 +0100

    Tech task/46 add linting (#81)
    * Added tslint to project
    
    * Modified WebcamFeed.tsx to be tslint compliant
    
    * Added prettier config for autoformatting
    
    * Updated file formatting, and fixed easy linting errors
    
    * Added rule exclusions for lambda functions in affected files
    
    * Added linting to CI
    
    * fixed config.yml
    
    * fixed config.yml
    
    * Fixed config.yml
    
    * fixed indenting linting check in config.yml
    
    * Fixed indenting on cache restore config.yml
    
    * added paths for save_cache
    
    * Changed linter to disallow public acess modifier
    
    * rebased and fixed fudged merge
    
    * fixed linting errors introduced by rebase
    
    * fixed file casing error
    
    * fixed casing of configMenu folder
    
    * Attempted fix for configMenu folder #5621
    
    * TechTask/46-add-linting (#81)
    
    Added linting
    
    * merged changes from master and updated formatting

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,11 @@
+import jsdom from 'jsdom';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import jsdom from 'jsdom';
 import App from './App';
 
 it('renders without crashing', () => {
-  const window = new jsdom.JSDOM(`...`, {url: "http://localhost"}).window;
-  const div = document.createElement('div');
-  ReactDOM.render(<App environment={window} />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const window = new jsdom.JSDOM(`...`, { url: 'http://localhost' }).window;
+    const div = document.createElement('div');
+    ReactDOM.render(<App environment={window} />, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
--- tests/getNormalisedCentrePointOf.test.js
+++ tests/getNormalisedCentrePointOf.test.js
@@ -1,20 +1,21 @@
-const getNormalisedCentrePointOf = require('../src/static/js/frame_feed').getNormalisedCentrePointOf;
-var video_1920_1080 = {width: 1920, height: 1080};
+const getNormalisedCentrePointOf = require('../src/static/js/frame_feed')
+    .getNormalisedCentrePointOf;
+var video_1920_1080 = { width: 1920, height: 1080 };
 describe('getNormalisedCentrePointof', () => {
     it('bounding box is point in bottom left corner, so is centre point', () => {
         var coords = [0, 0, 0, 0];
         point = getNormalisedCentrePointOf(coords, video_1920_1080);
-        expect(point).toStrictEqual([-1,-1]);
+        expect(point).toStrictEqual([-1, -1]);
     });
     it('bounding box spans screen, so point is in centre of screen', () => {
         var coords = [0, 0, 1920, 1080];
         point = getNormalisedCentrePointOf(coords, video_1920_1080);
-        expect(point).toStrictEqual([0,0]);
+        expect(point).toStrictEqual([0, 0]);
     });
     it('bounding box is point in exact middle of screen, so centre point is in centre of screen', () => {
         var coords = [960, 540, 0, 0];
         point = getNormalisedCentrePointOf(coords, video_1920_1080);
-        expect(point).toStrictEqual([0,0]);
+        expect(point).toStrictEqual([0, 0]);
     });
     it('centre point does not have integer coordinates', () => {
         var coords = [500, 500, 1000, 20];
