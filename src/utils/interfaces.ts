4e941f5faa22cf4cd5430dd02f2b5b2250d4da53
Jerome <ji8@hw.ac.uk>
2019-06-25 15:29:28 +0100

    refactored object detection to be modular
    returning Detection as own datatype
    
    added fascade for object detection and added objectSelection
    
    refactored eye position calculation
    
    fixed detection interfaces and added module implementations to app

---
+++ src/utils/interfaces.ts
@@ -0,0 +1,23 @@
+import { Bbox, DetectionConfig, DetectionImage, ModelConfig } from './types';
+
+export interface IObjectDetector {
+    detect(
+        image: DetectionImage,
+        detectionConfig?: DetectionConfig,
+    ): Promise<IDetection[]>;
+}
+
+export interface IDetection {
+    bbox: Bbox;
+    info: ICocoInfo;
+}
+
+export interface ICocoInfo {
+    certainty: number;
+    type: string;
+}
+
+export interface ICoords {
+    x: number;
+    y: number;
+}
