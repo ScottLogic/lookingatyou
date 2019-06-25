a3b23fb9e515db92a7f9daa249d1f0c06cb0fa3f
Jerome Illgner <ji8@hw.ac.uk>
2019-06-25 13:00:19 +0100

    Tech task/46 add linting (#81)
    * Added tslint to project
    
    * Modified WebcamFeed.tsx to be tslint compliant
    
    * Added prettier config for autoformatting
    
    * Updated file formatting, and fixed easy linting errors
    
    * Added rule exclusions for lambda functions in affected files
    
    * Added linting to CI
    
    * fixed config.yml
    
    * fixed config.yml
    
    * Fixed config.yml
    
    * fixed indenting linting check in config.yml
    
    * Fixed indenting on cache restore config.yml
    
    * added paths for save_cache
    
    * Changed linter to disallow public acess modifier
    
    * rebased and fixed fudged merge
    
    * fixed linting errors introduced by rebase
    
    * fixed file casing error
    
    * fixed casing of configMenu folder
    
    * Attempted fix for configMenu folder #5621
    
    * TechTask/46-add-linting (#81)
    
    Added linting
    
    * merged changes from master and updated formatting

---
+++ src/components/configMenu/ConfigMenu.tsx
@@ -0,0 +1,57 @@
+/* tslint:disable: jsx-no-lambda */
+
+import React from 'react';
+import './ConfigMenu.css';
+
+interface IConfigMenuProps {
+    width: string;
+    timerLength: number;
+    children: React.ReactNode;
+}
+interface IConfigMenuState {
+    leftPosition: string;
+    isUnderMouse: boolean;
+}
+
+export default class ConfigMenu extends React.Component<
+    IConfigMenuProps,
+    IConfigMenuState
+> {
+    private hideTimeout: number = 0;
+    constructor(props: IConfigMenuProps) {
+        super(props);
+        this.state = { leftPosition: '0px', isUnderMouse: false };
+        window.addEventListener('mousemove', () => {
+            this.setState({ leftPosition: '0px' });
+            clearInterval(this.hideTimeout);
+            if (!this.state.isUnderMouse) {
+                this.hideTimeout = window.setTimeout(
+                    () => this.setState({ leftPosition: '-' + props.width }),
+                    props.timerLength,
+                );
+            }
+        });
+    }
+    render() {
+        return (
+            <div
+                style={{
+                    width: this.props.width,
+                    left: this.state.leftPosition,
+                }}
+                className={'ConfigMenu'}
+                onMouseMove={() => {
+                    clearInterval(this.hideTimeout);
+                    this.setState({ isUnderMouse: true });
+                }}
+                onMouseLeave={() => {
+                    this.setState({ isUnderMouse: false });
+                }}
+            >
+                {' '}
+                <h1>Config</h1>
+                {this.props.children}
+            </div>
+        );
+    }
+}
