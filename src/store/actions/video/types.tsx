c8670007b8c4f304c5607aadc3d992c5aab0dbb9
Marcin Szczot <mszczot@scottlogic.com>
2019-06-25 15:48:37 +0100

    fixed indentation

--- src/store/actions/video/types.tsx
+++ src/store/actions/video/types.tsx
@@ -3,30 +3,30 @@ export const SET_VIDEO_STREAMS = 'SET_VIDEO_STREAMS';
 export const SET_DIMENSIONS = 'SET_DIMENSIONS';
 
 export interface IVideo {
-  deviceId: string,
-  video?: HTMLVideoElement | undefined,
-  width: number,
-  height: number,
-  stream: MediaStream | undefined,
+    deviceId: string;
+    video?: HTMLVideoElement | undefined;
+    width: number;
+    height: number;
+    stream: MediaStream | undefined;
 }
 
 export interface IVideoState {
-  videos: {[deviceId: string]: IVideo},
+    videos: { [deviceId: string]: IVideo };
 }
 
 export interface ISetVideoPayload {
-  video: HTMLVideoElement,
-  deviceId: string,
+    video: HTMLVideoElement;
+    deviceId: string;
 }
 
 interface ISetVideoAction {
-  type: typeof SET_VIDEO,
-  payload: ISetVideoPayload,
+    type: typeof SET_VIDEO;
+    payload: ISetVideoPayload;
 }
 
 interface ISetVideoStreamsAction {
-  type: typeof SET_VIDEO_STREAMS,
-  videos: IVideo[],
+    type: typeof SET_VIDEO_STREAMS;
+    videos: IVideo[];
 }
 
 export type VideoActionTypes = ISetVideoAction | ISetVideoStreamsAction;
