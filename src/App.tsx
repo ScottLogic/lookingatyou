bc66851d59008c95c06a8c235bdba00dd778f342
Ali Colver <acolver@scottlogic.com>
2019-06-27 11:48:28 +0100

    Fixing missing logic swap in hasTargetLeft

--- src/App.tsx
+++ src/App.tsx
@@ -256,7 +256,10 @@ export class App extends React.Component<AppProps, IAppState> {
         }
 
         if (this.state.isSquinting && Math.random() < 0.1) {
-            this.setState({ eyesOpenCoefficient: eyelidPosition.OPEN });
+            this.setState({
+                eyesOpenCoefficient: eyelidPosition.OPEN,
+                isSquinting: false,
+            });
         }
     }
 
