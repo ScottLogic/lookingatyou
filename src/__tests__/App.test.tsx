97508d83c3db286e5ec7bcaad1e215b6360c17e9
Marcin Szczot <mszczot@scottlogic.com>
2019-06-27 13:44:54 +0100

    added test to check if app renders Video component for each deviceId

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,20 +1,44 @@
+import { expression } from '@babel/template';
+import { mount, shallow } from 'enzyme';
 import jsdom from 'jsdom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from '../App';
+import { testDevice1, testDevice2 } from './reducers/video/videoReducer.test';
 
-it('renders without crashing', () => {
-    const window = new jsdom.JSDOM(`...`, { url: 'http://localhost' }).window;
-    const div = document.createElement('div');
-    const configureStream = jest.fn();
-    ReactDOM.render(
-        <App
-            deviceIds={[]}
-            environment={window}
-            configureStream={configureStream}
-            videos={[]}
-        />,
-        div,
-    );
-    ReactDOM.unmountComponentAtNode(div);
+let window: Window;
+let div: HTMLElement;
+let configureStream: jest.Mock;
+
+describe('App', () => {
+    beforeEach(() => {
+        window = new jsdom.JSDOM(`...`, { url: 'http://localhost' }).window;
+        div = document.createElement('div');
+        configureStream = jest.fn();
+    });
+
+    it('renders without crashing', () => {
+        ReactDOM.render(
+            <App
+                deviceIds={[]}
+                environment={window}
+                configureStream={configureStream}
+                videos={[]}
+            />,
+            div,
+        );
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('should render 2 video components when 2 devices are detected', () => {
+        const wrapper = shallow(
+            <App
+                deviceIds={[testDevice1, testDevice2]}
+                environment={window}
+                configureStream={configureStream}
+                videos={[]}
+            />,
+        );
+        expect(wrapper.find('Connect(Video)')).toHaveLength(2);
+    });
 });
