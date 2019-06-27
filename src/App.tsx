e2f4e7f72bbba0a03113d4a1f881e97deb5e2fe9
Ali Colver <acolver@scottlogic.com>
2019-06-27 11:41:41 +0100

    Squint when someone leaves the frame

--- src/App.tsx
+++ src/App.tsx
@@ -37,6 +37,7 @@ interface IAppState {
     eyesOpenCoefficient: number;
     webcamAvailable: boolean;
     isBlinking: boolean;
+    isSquinting: boolean;
     userConfig: IUserConfig;
     targetX: number;
     targetY: number;
@@ -87,6 +88,7 @@ export class App extends React.Component<AppProps, IAppState> {
             eyesOpenCoefficient: eyelidPosition.OPEN,
             webcamAvailable: false,
             isBlinking: false,
+            isSquinting: false,
             targetX: this.props.environment.innerWidth / 4,
             targetY: this.props.environment.innerHeight / 2,
             direction: true,
@@ -247,9 +249,14 @@ export class App extends React.Component<AppProps, IAppState> {
 
     hasTargetLeft() {
         if (this.state.personDetected) {
-            this.setState({ personDetected: false });
+            this.setState({ personDetected: false, isSquinting: true });
             this.setDilation(pupilSizes.constricted);
             this.setDilation(pupilSizes.neutral);
+            this.setState({ eyesOpenCoefficient: 0.2 });
+        }
+
+        if (this.state.isSquinting && Math.random() < 0.1) {
+            this.setState({ eyesOpenCoefficient: eyelidPosition.OPEN });
         }
     }
 
