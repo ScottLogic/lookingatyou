7aeac088af00bebae6fe639613f621f0bfb68627
Ali Colver <acolver@scottlogic.com>
2019-06-27 12:43:25 +0100

    Moving dialtion offset and multiplier out into constants file

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,12 @@ import {
     colours,
     configStorageKey,
     defaultConfigValues,
+    dilationMultipler,
+    dilationOffset,
     eyelidPosition,
     eyes,
     FPS,
+    maxBrightness,
     middleX,
     middleY,
     pupilSizes,
@@ -352,12 +355,12 @@ export class App extends React.Component<AppProps, IAppState> {
                 colorSum / (canvas.width * canvas.height),
             );
 
-            if (brightness > 220) {
+            if (brightness > maxBrightness) {
                 this.setState({
                     eyesOpenCoefficient: eyelidPosition.CLOSED,
                     tooBright: true,
                 });
-                brightness = 220;
+                brightness = maxBrightness;
             } else if (this.state.tooBright) {
                 this.setState({
                     eyesOpenCoefficient: eyelidPosition.OPEN,
@@ -365,7 +368,10 @@ export class App extends React.Component<AppProps, IAppState> {
                 });
             }
 
-            const scaledPupilSize = ((220 - brightness) / 220) * 0.7 + 0.8;
+            const scaledPupilSize =
+                ((maxBrightness - brightness) / maxBrightness) *
+                    dilationMultipler +
+                dilationOffset;
 
             callback(scaledPupilSize);
         }
