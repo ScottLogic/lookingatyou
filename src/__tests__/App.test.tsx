e902cba4fceb3db6ffbfba4ed41901f6e4f4ad6d
Adam McDevitt <amcdevitt@scottlogic.com>
2019-07-01 09:20:12 +0100

    fixed App.test.tsx

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,12 +2,19 @@ import { shallow } from 'enzyme';
 import jsdom from 'jsdom';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from '../App';
+import { Provider } from 'react-redux';
+import { AnyAction, Store } from 'redux';
+import configureStore from 'redux-mock-store';
+import App from '../App';
+import { App as App2 } from '../App';
+import { initialState as initialConfigState } from '../store/reducers/configReducer';
+import { IRootStore } from '../store/reducers/rootReducer';
+import { initialState as initialVideoState } from '../store/reducers/videoReducer';
 import { testDevice1, testDevice2 } from './reducers/video/videoReducer.test';
-
 let window: Window;
 let div: HTMLElement;
 let configureStream: jest.Mock;
+let store: Store<any, AnyAction>;
 
 describe('App', () => {
     beforeEach(() => {
@@ -17,13 +24,17 @@ describe('App', () => {
     });
 
     it('renders without crashing', () => {
+        const mockStore = configureStore();
+        const initialState: IRootStore = {
+            videoStore: initialVideoState,
+            configStore: initialConfigState,
+        };
+        store = mockStore(initialState);
+        configureStream = jest.fn();
         ReactDOM.render(
-            <App
-                deviceIds={[]}
-                environment={window}
-                configureStream={configureStream}
-                videos={[]}
-            />,
+            <Provider store={store}>
+                <App environment={window} configureStream={configureStream} />
+            </Provider>,
             div,
         );
         ReactDOM.unmountComponentAtNode(div);
@@ -31,11 +42,12 @@ describe('App', () => {
 
     it('should render 2 video components when 2 devices are detected', () => {
         const wrapper = shallow(
-            <App
+            <App2
                 deviceIds={[testDevice1, testDevice2]}
                 environment={window}
                 configureStream={configureStream}
                 videos={[]}
+                config={initialConfigState.config}
             />,
         );
         expect(wrapper.find('Connect(Video)')).toHaveLength(2);
