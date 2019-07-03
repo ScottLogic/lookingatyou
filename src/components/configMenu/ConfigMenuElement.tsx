00961e4e5e37b69c525081e56f5d8edd97e6d4f9
Adam McDevitt <amcdevitt@scottlogic.com>
2019-07-03 14:53:46 +0100

    added back accidentally deleted performance improvements

--- src/components/configMenu/ConfigMenuElement.tsx
+++ src/components/configMenu/ConfigMenuElement.tsx
@@ -44,84 +44,89 @@ type ConfigMenuElementProps = IConfigMenuElementProps &
     IConfigMenuElementMapStateToProps &
     IConfigMenuElementMapDispatchToProps;
 
-function ConfigMenuElement(props: ConfigMenuElementProps) {
-    function parseAndStoreXSensitivity(xSensitivity: string) {
-        props.setConfig({
-            partialConfig: { xSensitivity: parseFloat(xSensitivity) },
-        });
-    }
-    function parseAndStoreYSensitivity(ySensitivity: string) {
-        props.setConfig({
-            partialConfig: { ySensitivity: parseFloat(ySensitivity) },
-        });
-    }
-    function parseAndStoreFPS(fps: string) {
-        props.setConfig({ partialConfig: { fps: parseInt(fps, 10) } });
-    }
-    function storeSwapEyes(swapEyes: boolean) {
-        props.setConfig({ partialConfig: { swapEyes } });
-    }
-    function storeToggleDebug(toggleDebug: boolean) {
-        props.setConfig({ partialConfig: { toggleDebug } });
-    }
-    function storeIrisColor(irisColor: string) {
-        props.setConfig({ partialConfig: { irisColor } });
-    }
-    function extractFloatToString(floatString: string): string {
-        return `${parseFloat(floatString)}`;
-    }
-    function extractIntToString(intString: string): string {
-        return `${parseInt(intString, 10)}`;
-    }
-    function isValidSensitivity(sensitivity: string): boolean {
-        return !isNaN(parseFloat(sensitivity)) && parseFloat(sensitivity) >= 0;
-    }
-    function isValidFPS(fps: string): boolean {
-        return !isNaN(parseInt(fps, 10)) && parseInt(fps, 10) > 0;
-    }
-    return (
-        <ConfigMenu width="14em" timerLength={1000} window={props.window}>
-            <TextBoxMenuItem
-                name={'X Sensitivity'}
-                defaultValue={`${props.config.xSensitivity}`}
-                isValidInput={isValidSensitivity}
-                onValidInput={parseAndStoreXSensitivity}
-                parse={extractFloatToString}
-            />
-            <TextBoxMenuItem
-                name={'Y Sensitivity'}
-                defaultValue={`${props.config.ySensitivity}`}
-                isValidInput={isValidSensitivity}
-                onValidInput={parseAndStoreYSensitivity}
-                parse={extractFloatToString}
-            />
-            <TextBoxMenuItem
-                name={'FPS'}
-                defaultValue={`${props.config.fps}`}
-                isValidInput={isValidFPS}
-                onValidInput={parseAndStoreFPS}
-                parse={extractIntToString}
-            />
-            <CheckBoxMenuItem
-                name={'Swap Eyes'}
-                checked={props.config.swapEyes}
-                onInputChange={storeSwapEyes}
-            />
-            <CheckBoxMenuItem
-                name={'Toggle Debug'}
-                checked={props.config.toggleDebug}
-                onInputChange={storeToggleDebug}
-            />
-            <ColorMenuItem
-                name={'Iris Color'}
-                color={props.config.irisColor}
-                onInputChange={storeIrisColor}
-            />
-            <CanvasMenuItem name={'Left Camera'} />
-            <CanvasMenuItem name={'Right Camera'} />
-        </ConfigMenu>
-    );
-}
+const ConfigMenuElement = React.memo(
+    (props: ConfigMenuElementProps) => {
+        function parseAndStoreXSensitivity(xSensitivity: string) {
+            props.setConfig({
+                partialConfig: { xSensitivity: parseFloat(xSensitivity) },
+            });
+        }
+        function parseAndStoreYSensitivity(ySensitivity: string) {
+            props.setConfig({
+                partialConfig: { ySensitivity: parseFloat(ySensitivity) },
+            });
+        }
+        function parseAndStoreFPS(fps: string) {
+            props.setConfig({ partialConfig: { fps: parseInt(fps, 10) } });
+        }
+        function storeSwapEyes(swapEyes: boolean) {
+            props.setConfig({ partialConfig: { swapEyes } });
+        }
+        function storeToggleDebug(toggleDebug: boolean) {
+            props.setConfig({ partialConfig: { toggleDebug } });
+        }
+        function storeIrisColor(irisColor: string) {
+            props.setConfig({ partialConfig: { irisColor } });
+        }
+        function extractFloatToString(floatString: string): string {
+            return `${parseFloat(floatString)}`;
+        }
+        function extractIntToString(intString: string): string {
+            return `${parseInt(intString, 10)}`;
+        }
+        function isValidSensitivity(sensitivity: string): boolean {
+            return (
+                !isNaN(parseFloat(sensitivity)) && parseFloat(sensitivity) >= 0
+            );
+        }
+        function isValidFPS(fps: string): boolean {
+            return !isNaN(parseInt(fps, 10)) && parseInt(fps, 10) > 0;
+        }
+        return (
+            <ConfigMenu width="14em" timerLength={1000} window={props.window}>
+                <TextBoxMenuItem
+                    name={'X Sensitivity'}
+                    defaultValue={`${props.config.xSensitivity}`}
+                    isValidInput={isValidSensitivity}
+                    onValidInput={parseAndStoreXSensitivity}
+                    parse={extractFloatToString}
+                />
+                <TextBoxMenuItem
+                    name={'Y Sensitivity'}
+                    defaultValue={`${props.config.ySensitivity}`}
+                    isValidInput={isValidSensitivity}
+                    onValidInput={parseAndStoreYSensitivity}
+                    parse={extractFloatToString}
+                />
+                <TextBoxMenuItem
+                    name={'FPS'}
+                    defaultValue={`${props.config.fps}`}
+                    isValidInput={isValidFPS}
+                    onValidInput={parseAndStoreFPS}
+                    parse={extractIntToString}
+                />
+                <CheckBoxMenuItem
+                    name={'Swap Eyes'}
+                    checked={props.config.swapEyes}
+                    onInputChange={storeSwapEyes}
+                />
+                <CheckBoxMenuItem
+                    name={'Toggle Debug'}
+                    checked={props.config.toggleDebug}
+                    onInputChange={storeToggleDebug}
+                />
+                <ColorMenuItem
+                    name={'Iris Color'}
+                    color={props.config.irisColor}
+                    onInputChange={storeIrisColor}
+                />
+                <CanvasMenuItem name={'Left Camera'} />
+                <CanvasMenuItem name={'Right Camera'} />
+            </ConfigMenu>
+        );
+    },
+    (previous, next) => previous.config === next.config,
+);
 
 export default connect(
     mapStateToProps,
