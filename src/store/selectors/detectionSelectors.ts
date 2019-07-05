bb2c7e64c2fe0a7ab2c2abce35d7cd0ef2cf0019
Marcin Szczot <mszczot@scottlogic.com>
2019-07-05 11:30:51 +0100

    rewrote getBbox method in selector to use find instead of filter

--- src/store/selectors/detectionSelectors.ts
+++ src/store/selectors/detectionSelectors.ts
@@ -2,11 +2,9 @@ import { Bbox } from '../../utils/types';
 import { IRootStore } from '../reducers/rootReducer';
 
 export function getBbox(state: IRootStore): Bbox {
-    const people = state.detectionStore.detections.filter(
+    const person = state.detectionStore.detections.find(
         detection => detection.info.type === 'person',
     );
 
-    if (people.length > 0) {
-        return people[0].bbox;
-    }
+    return person ? person.bbox : undefined;
 }
