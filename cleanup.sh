b686412be3dedcbac7f741c2196f14193549d360
Marcin Szczot <mszczot@scottlogic.com>
2019-07-05 14:24:59 +0100

    created cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,22 +1,33 @@
+deleteBranch() {
+    aws s3 rm s3://looking-at-you/feature/$1
+}
+
+formatBranch() {
+    $1 | cut -d'/' -f 3
+}
+
 # regex for feature branches
 REGEX="\([Ff]eature\)\/[0-9a-zA-Z\-]*"
 
 # get all remote feature branches
-ALL_BRANCHES="$(git branch -r | grep ${REGEX})"
+ALL_BRANCHES="$(git branch -r | grep $REGEX | cut -d'/' -f 3)"
+MY_BRANCHES=($ALL_BRANCHES)
+
 # get all merged feature branches from the git log
 MERGED_BRANCHES="$(git log | grep ${REGEX})"
 
 # get the list of deployed branches
 DEPLOYED_BRANCHES="$(aws s3 ls s3://looking-at-you/feature/)"
 
-for branch in $ALL_BRANCHES; do
-    echo $branch
-done
+for DEPLOYED_BRANCH in $DEPLOYED_BRANCHES; do
+    if [[ "($DEPLOYED_BRANCH)" != *PRE* ]]; then
+        # check for merged branches
 
-for branch in $MERGED_BRANCHES; do
-    echo $branch
-done
+        echo $MERGED_BRANCHES
 
-for branch in $DEPLOYED_BRANCHES; do
-    echo $branch
+        # check for deleted and not merged branches
+        if [[ ! "${MY_BRANCHES[@]}" =~ "${DEPLOYED_BRANCH::-1}" ]]; then
+            deleteBranch $DEPLOYED_BRANCH
+        fi
+    fi
 done
