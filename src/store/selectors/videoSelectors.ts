51bade6eab8bab19563a9fd670c4f389aa1ba35a
Adam McDevitt <amcdevitt@scottlogic.com>
2019-07-08 14:44:26 +0100

    swap eyes checkbox on config menu now functional

--- src/store/selectors/videoSelectors.ts
+++ src/store/selectors/videoSelectors.ts
@@ -2,16 +2,20 @@ import { IVideo } from '../actions/video/types';
 import { IRootStore } from '../reducers/rootReducer';
 
 export function getDeviceIds(state: IRootStore): string[] {
-    return Object.keys(state.videoStore.videos).map(
+    const deviceIds = Object.keys(state.videoStore.videos).map(
         (deviceId: string) => deviceId,
     );
+    if (state.configStore.config.swapEyes) {
+        deviceIds.reverse();
+    }
+    return deviceIds;
 }
 
 export function getStreamForDevice(
     state: IRootStore,
     deviceId: string,
 ): IVideo {
-    return Object.keys(state.videoStore.videos)
+    return getDeviceIds(state)
         .filter((key: string) => key === deviceId)
         .map((key: string) => state.videoStore.videos[key])[0];
 }
@@ -19,7 +23,8 @@ export function getStreamForDevice(
 export function getVideos(
     state: IRootStore,
 ): Array<HTMLVideoElement | undefined> {
-    return Object.values(state.videoStore.videos).map(
-        (item: IVideo) => item.video,
+    const videos = getDeviceIds(state).map(
+        (deviceId: string) => state.videoStore.videos[deviceId].video,
     );
+    return videos;
 }
