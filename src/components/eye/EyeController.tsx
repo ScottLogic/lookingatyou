4e5da97cb7ef74bcf3885440dae7a1ca8929d42a
Ali Colver <acolver@scottlogic.com>
2019-07-09 12:06:56 +0100

    Fixing warnings

--- src/components/eye/EyeController.tsx
+++ src/components/eye/EyeController.tsx
@@ -41,9 +41,9 @@ export const EyeController = React.memo(
         const [isBlinking, setIsBlinking] = useState(false); // Will change based on camera feed e.g. blink less when object in frame
         const [eyesOpenCoefficient] = useState(eyelidPosition.OPEN); // Will change based on camera feed e.g. higher coefficient to show surprise
         const [dilationCoefficient] = useState(pupilSizes.neutral); // Will change based on camera feed e.g. briefly increase coefficient (dilate) when object enters frame then reset to 1 (neutral)
-        const [fatigueMultiplier] = useState(getFatigueMultiplier());
 
         useEffect(() => {
+            const fatigueMultiplier = getFatigueMultiplier();
             const blink = props.environment.setInterval(() => {
                 if (isBlinking) {
                     setIsBlinking(false);
