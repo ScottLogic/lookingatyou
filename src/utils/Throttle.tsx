6c903e53627c7ff7c381f98d3a43638df140e7d1
Adam McDevitt <amcdevitt@scottlogic.com>
2019-07-15 10:57:02 +0100

    added Throttle higher-order-component

---
+++ src/utils/Throttle.tsx
@@ -0,0 +1,25 @@
+import { throttle, ThrottleSettings } from 'lodash';
+import React from 'react';
+
+export default function Throttle<Props>(
+    Component: React.ComponentType<Props>,
+    wait?: number | undefined,
+    options?: ThrottleSettings | undefined,
+): React.ComponentType<Props> {
+    return class Throttled extends React.Component<Props> {
+        updateThrottled = throttle(this.forceUpdate, wait, options);
+
+        shouldComponentUpdate() {
+            this.updateThrottled();
+            return false;
+        }
+
+        componentWillUnmount() {
+            this.updateThrottled.cancel();
+        }
+
+        render() {
+            return <Component {...this.props} />;
+        }
+    };
+}
