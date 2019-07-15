7edd8d2b992c16a26680f94df343c0d2004b435b
Marcin Szczot <mszczot@scottlogic.com>
2019-07-15 11:32:41 +0100

    load model on componentDidUpdate

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { IObjectDetector } from './models/objectDetection';
 import { loadModel } from './store/actions/detections/actions';
 import { IRootStore } from './store/reducers/rootReducer';
 import { getWebcamAvailable } from './store/selectors/videoSelectors';
+import Posenet from './utils/objectDetection/posenet';
 
 interface IAppState {
     width: number;
@@ -66,6 +67,12 @@ export class App extends React.PureComponent<AppProps, IAppState> {
             'resize',
             this.updateDimensions,
         );
+    }
+
+    componentDidUpdate() {
+        if (this.props.webcamAvailable && !this.props.model) {
+            this.props.loadModel(Posenet.init);
+        }
     }
 
     componentWillUnmount() {
