dfec40e883602b1a7df8ca512051faf86daab4c8
Ali Colver <acolver@scottlogic.com>
2019-07-26 09:26:45 +0100

    Lowering confidence threshold

--- src/AppConstants.ts
+++ src/AppConstants.ts
@@ -40,6 +40,6 @@ export const bodyParts = {
 };
 export const canvasScale = 1;
 export const canvasLineWidth = 5;
-export const minConfidence = 0.4;
+export const minConfidence = 0.2;
 export const yOffset = 10;
 export const xOffset = 10;
