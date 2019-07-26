0d019193060a960fc9f1f53c6c73a59597782036
Jerome <ji8@hw.ac.uk>
2019-07-26 17:18:13 +0100

    fixed fudged merge and updated snapshot

--- src/components/configMenu/ConfigMenuElement.tsx
+++ src/components/configMenu/ConfigMenuElement.tsx
@@ -85,12 +85,24 @@ export const ConfigMenuElement = React.memo(
 
                     <br />
                     <CheckBoxMenuItem
-                        name={'Show Target'}
+                        name={'Show Reflect'}
                         configName={'toggleReflection'}
                         helpWith={HelpWith.REFLECTION}
                         checked={props.config.toggleReflection}
                         onInputChange={props.updateAppConfig}
                     />
+                    {props.config.toggleReflection && (
+                        <NumberMenuItem
+                            name={'Reflect Opacity'}
+                            configName={'reflectionOpacity'}
+                            step={0.01}
+                            defaultValue={props.config.reflectionOpacity}
+                            onValidInput={props.updateAppConfig}
+                            helpWith={HelpWith.REFLECTION_OPACITY}
+                            min={0.01}
+                            max={1.0}
+                        />
+                    )}
                     <CheckBoxMenuItem
                         name={'Toggle Debug'}
                         configName={'toggleDebug'}
@@ -248,97 +260,9 @@ export const ConfigMenuElement = React.memo(
                     ?
                 </span>
 
-<<<<<<< HEAD
-                <NumberMenuItem
-                    name={'FPS'}
-                    configName={'fps'}
-                    step={1}
-                    defaultValue={props.config.fps}
-                    onValidInput={props.setConfig}
-                    helpWith={HelpWith.FPS}
-                    min={1}
-                />
-
-                <br />
-
-                <NumberMenuItem
-                    name={'X Sensitivity'}
-                    configName={'xSensitivity'}
-                    step={0.1}
-                    defaultValue={props.config.xSensitivity}
-                    onValidInput={props.setConfig}
-                    helpWith={HelpWith.X_SENSITIVITY}
-                    min={0}
-                />
-                <NumberMenuItem
-                    name={'Y Sensitivity'}
-                    configName={'ySensitivity'}
-                    step={0.1}
-                    defaultValue={props.config.ySensitivity}
-                    onValidInput={props.setConfig}
-                    helpWith={HelpWith.Y_SENSITIVITY}
-                    min={0}
-                />
-
-                <br />
-
-                <ColorMenuItem
-                    name={'Iris Colour'}
-                    configName={'irisColor'}
-                    color={props.config.irisColor}
-                    onInputChange={props.setConfig}
-                    helpWith={HelpWith.IRIS_COLOUR}
-                />
-
-                <CheckBoxMenuItem
-                    name={'Show Reflect'}
-                    configName={'toggleReflection'}
-                    helpWith={HelpWith.REFLECTION}
-                    checked={props.config.toggleReflection}
-                    onInputChange={props.setConfig}
-                />
-                {props.config.toggleReflection && (
-                    <NumberMenuItem
-                        name={'Reflect Opacity'}
-                        configName={'reflectionOpacity'}
-                        step={0.01}
-                        defaultValue={props.config.reflectionOpacity}
-                        onValidInput={props.setConfig}
-                        helpWith={HelpWith.REFLECTION_OPACITY}
-                        min={0.01}
-                        max={1.0}
-                    />
-                )}
-
-                <CheckBoxMenuItem
-                    name={'Toggle Debug'}
-                    configName={'toggleDebug'}
-                    helpWith={HelpWith.DEBUG}
-                    checked={props.config.toggleDebug}
-                    onInputChange={props.setConfig}
-                />
-
-                {props.config.toggleDebug
-                    ? props.videos.map((ignore, index) => {
-                          return (
-                              <CanvasMenuItem
-                                  name={'Camera'}
-                                  key={index}
-                                  helpWith={HelpWith.VIDEO_STREAM}
-                                  videoIndex={index}
-                              />
-                          );
-                      })
-                    : null}
-
-                {Object.values(HelpWith).map((type, key: number) => (
-                    <Help key={key} problemWith={HelpWith[type] as HelpWith} />
-                ))}
-=======
                 {!props.config.toggleDebug
                     ? renderUserConfig()
                     : renderDevConfig()}
->>>>>>> Added developer config menu for models
             </ConfigMenu>
         );
     },
