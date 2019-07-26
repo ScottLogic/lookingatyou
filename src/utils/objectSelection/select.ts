44251b5661547e8e7079bca5dd3b12960c0cb23a
Ali Colver <acolver@scottlogic.com>
2019-07-26 11:51:15 +0100

    refactor

--- src/utils/objectSelection/select.ts
+++ src/utils/objectSelection/select.ts
@@ -184,12 +184,12 @@ export function closerToColour(
 }
 
 export function getXStart(keypoints: Keypoint[]) {
-    const rightShoulder = keypoints.filter(
+    const rightShoulder = keypoints.find(
         keypoint => keypoint.part === bodyParts.RIGHT_SHOULDER,
-    )[0];
-    const leftShoulder = keypoints.filter(
+    );
+    const leftShoulder = keypoints.find(
         keypoint => keypoint.part === bodyParts.LEFT_SHOULDER,
-    )[0];
+    );
     return leftShoulder && rightShoulder
         ? Math.abs(
               Math.round(
@@ -201,9 +201,9 @@ export function getXStart(keypoints: Keypoint[]) {
 }
 
 export function getYStart(keypoints: Keypoint[]) {
-    const rightShoulder = keypoints.filter(
+    const rightShoulder = keypoints.find(
         keypoint => keypoint.part === bodyParts.RIGHT_SHOULDER,
-    )[0];
+    );
     return rightShoulder ? Math.round(rightShoulder.position.y) + yOffset : 0;
 }
 
