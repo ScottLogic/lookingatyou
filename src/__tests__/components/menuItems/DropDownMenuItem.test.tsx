e7b6c17a8fff7af7eb4a4a1c97b449639c5a5c6a
Adam McDevitt <amcdevitt@scottlogic.com>
2019-07-30 11:32:06 +0100

    wrote tests

---
+++ src/__tests__/components/menuItems/DropDownMenuItem.test.tsx
@@ -0,0 +1,36 @@
+import { mount, ReactWrapper, shallow } from 'enzyme';
+import React from 'react';
+import Help, { HelpWith } from '../../../components/configMenu/Help';
+import DropDownMenuItem, {
+    IDropDownMenuItemProps,
+} from '../../../components/configMenu/menuItems/DropDownMenuItem';
+import NumberMenuItem, {
+    INumberMenuItemProps,
+} from '../../../components/configMenu/menuItems/NumberMenuItem';
+
+let props: IDropDownMenuItemProps;
+
+describe('NumberMenuItem tests', () => {
+    beforeEach(() => {
+        props = {
+            name: 'test',
+            configName: 'test',
+            onInputChange: jest.fn(),
+            values: ['a', 'b', 'c'],
+            defaultValue: 'a',
+            helpWith: HelpWith.OUTPUT_STRIDE,
+        };
+    });
+
+    it('should render correctly', () => {
+        const wrapper = shallow(<DropDownMenuItem {...props} />).debug();
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should call onInputChange when input is changed', () => {
+        const value = 'b';
+        const wrapper = mount(<DropDownMenuItem {...props} />);
+        wrapper.find('select').simulate('change', { target: { value } });
+        expect(wrapper.get(0).props.onInputChange).toHaveBeenCalled();
+    });
+});
