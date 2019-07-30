0a0bf42690f12a37db5015f153701603642fff83
Jerome <ji8@hw.ac.uk>
2019-07-30 12:04:30 +0100

    added selectors, set timeout on environment, started changing eye movement

--- src/components/eye/utils/MovementUtils.ts
+++ src/components/eye/utils/MovementUtils.ts
@@ -1,6 +1,7 @@
 import {
     eyeCoords,
     idleMovementConsts,
+    irisSkewFactor,
     lightConsts,
 } from '../../../AppConstants';
 
@@ -53,11 +54,9 @@ function moveEye(
     currentX: number,
     isMovingLeft: boolean,
 ): { newX: number; isMovingLeft: boolean } {
-    if (isMovingLeft) {
-        return moveLeft(currentX, isMovingLeft);
-    } else {
-        return moveRight(currentX, isMovingLeft);
-    }
+    return isMovingLeft
+        ? moveLeft(currentX, isMovingLeft)
+        : moveRight(currentX, isMovingLeft);
 }
 
 function moveLeft(
@@ -88,4 +87,62 @@ function moveRight(
         };
     }
     return { newX: currentX, isMovingLeft };
+}
+
+export function getMaxDisplacement(scleraRadius: number, irisRadius: number) {
+    return (scleraRadius - irisRadius * irisSkewFactor) / irisSkewFactor;
+}
+
+export function getIrisAdjustment(
+    x: number,
+    y: number,
+    height: number,
+    width: number,
+    scleraRadius: number,
+    irisRadius: number,
+    previousAngle: number = 0,
+) {
+    const displacement = Math.hypot(x - width / 2, y - height / 2);
+    const maxDisplacement = getMaxDisplacement(scleraRadius, irisRadius);
+
+    const scale =
+        irisSkewFactor +
+        ((1 - irisSkewFactor) * (maxDisplacement - displacement)) /
+            maxDisplacement;
+
+    let angle = (Math.atan2(y - height / 2, x - width / 2) * 180) / Math.PI;
+
+    if (angle - previousAngle < -90) {
+        angle = angle + 180;
+    } else if (angle - previousAngle > 90) {
+        angle = angle - 180;
+    }
+
+    return {
+        scale,
+        angle,
+    };
+}
+
+export function generateInnerPath(radius: number, sectors: number) {
+    const innerRadius = radius * 0.1;
+    const outerRadius = radius * 0.9;
+    const radianStep = (2 * Math.PI) / sectors;
+    const innerOffset = -radianStep / 2;
+
+    let currInnerPath = 'M 0 0';
+    for (let i = 0; i < sectors; i++) {
+        const currRadianStep = radianStep * i;
+        const lineOut = `L ${outerRadius *
+            Math.cos(currRadianStep + innerOffset)} ${outerRadius *
+            Math.sin(currRadianStep + innerOffset)}`;
+
+        const lineIn = `L ${innerRadius *
+            Math.cos(currRadianStep)} ${innerRadius *
+            Math.sin(currRadianStep)}`;
+
+        currInnerPath += lineOut;
+        currInnerPath += lineIn;
+    }
+    return currInnerPath;
 }
