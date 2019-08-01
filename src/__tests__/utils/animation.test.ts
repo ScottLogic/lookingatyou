503d16aec6ff030dfb054806357ec3ac70aed07d
Ali Colver <acolver@scottlogic.com>
2019-08-01 12:13:48 +0100

    :white_check_mark: Updating based on comments
     #357

--- src/__tests__/utils/animation.test.ts
+++ src/__tests__/utils/animation.test.ts
@@ -1,33 +1,12 @@
-import {
-    dab,
-    leftWink,
-    rightWink,
-    rollEyes,
-    shock,
-} from '../../utils/pose/animations';
+import { dab, rollEyes } from '../../utils/pose/animations';
 
 describe('animations should have set number of frames', () => {
-    const right = rightWink();
-    const left = leftWink();
     const roll = rollEyes();
-    const shocked = shock();
     const dabbing = dab();
 
-    it('rightWink should have 2 frames', () => {
-        expect(right.length).toEqual(2);
-        expect(Array.isArray(right)).toBeTruthy();
-    });
-    it('leftWink should have 2 frames', () => {
-        expect(left.length).toEqual(2);
-        expect(Array.isArray(left)).toBeTruthy();
-    });
     it('eyeRoll should have 17 frames', () => {
         expect(roll.length).toEqual(17);
         expect(Array.isArray(roll)).toBeTruthy();
-    });
-    it('shock should have 5 frames', () => {
-        expect(shocked.length).toEqual(5);
-        expect(Array.isArray(shocked)).toBeTruthy();
     });
     it('dab should have 20 frames', () => {
         expect(dabbing.length).toEqual(20);
