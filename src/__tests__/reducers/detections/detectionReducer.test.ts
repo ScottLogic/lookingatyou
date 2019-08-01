3b1f31c578b1b82c86e0820045e8a0da052ec871
Adam McDevitt <amcdevitt@scottlogic.com>
2019-08-01 13:27:33 +0100

    :white_check_mark: added tests
    added tests for updating history, swapping selection, updating open coefficient and updating animation

--- src/__tests__/reducers/detections/detectionReducer.test.ts
+++ src/__tests__/reducers/detections/detectionReducer.test.ts
@@ -1,8 +1,14 @@
 import { eyelidPosition, targetingConsts } from '../../../AppConstants';
 import { IDetection } from '../../../models/objectDetection';
-import { setDetections } from '../../../store/actions/detections/actions';
+import {
+    setAnimation,
+    setDetections,
+    setOpen,
+    swapSelection,
+} from '../../../store/actions/detections/actions';
 import { IDetectionState } from '../../../store/actions/detections/types';
 import detectionStore from '../../../store/reducers/detectionReducer';
+import { Animation } from '../../../utils/pose/animations';
 import { IColour, ICoords, IHistory } from '../../../utils/types';
 
 describe('Detection Reducer Tests', () => {
@@ -17,9 +23,11 @@ describe('Detection Reducer Tests', () => {
         history: [],
     };
 
-    const testDetections: IDetection[] = [
-        { bbox: [1, 1, 1, 1], info: { keypoints: [], score: 5 } },
-    ];
+    const testDetection: IDetection = {
+        bbox: [1, 1, 1, 1],
+        info: { keypoints: [], score: 5 },
+    };
+    const testDetections: IDetection[] = [testDetection];
     const testTarget: ICoords = { x: 10, y: 15 };
     const testColour: IColour = { r: 20, g: 25, b: 30 };
     const testAction = setDetections({
@@ -60,5 +68,54 @@ describe('Detection Reducer Tests', () => {
         expect(detectionStore(initialState, testAction)).toStrictEqual(
             expected,
         );
+    });
+    it('history should be cleared on swap selection, and there should only be one detection', () => {
+        const nextSelectionSwapTime = 5000;
+        const action = swapSelection({
+            selection: testDetection,
+            nextSelectionSwapTime,
+        });
+        const expected = {
+            ...testState,
+            detections: testDetections,
+            history: [],
+            nextSelectionSwapTime,
+        };
+        expect(detectionStore(testState, action)).toStrictEqual(expected);
+    });
+
+    it('should update open coefficient correctly', () => {
+        const eyesOpenCoefficient = 0.3;
+        const action = setOpen(eyesOpenCoefficient);
+        const expected = {
+            ...testState,
+            eyesOpenCoefficient,
+        };
+        expect(detectionStore(testState, action)).toStrictEqual(expected);
+    });
+
+    it('should update animation correctly', () => {
+        const animation: Animation = [
+            {
+                normalisedCoords: { x: 5, y: 6 },
+                openCoefficient: 0.2,
+                dilation: 6,
+                irisColor: 'red',
+                duration: 10,
+            },
+            {
+                normalisedCoords: { x: 50, y: 60 },
+                openCoefficient: 0.02,
+                dilation: 56,
+                irisColor: 'red',
+                duration: 120,
+            },
+        ];
+        const action = setAnimation(animation);
+        const expected = {
+            ...testState,
+            animation,
+        };
+        expect(detectionStore(testState, action)).toStrictEqual(expected);
     });
 });
