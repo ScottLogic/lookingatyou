1becf0d6a536f553c48da7d9980dc421b9dbaed3
Adam McDevitt <amcdevitt@scottlogic.com>
2019-08-01 17:02:48 +0100

    :art: fixed ConfigMenu.test.tsx
    remove misplaced imports, removed irelevant declarations

--- src/__tests__/components/ConfigMenu.test.tsx
+++ src/__tests__/components/ConfigMenu.test.tsx
@@ -3,39 +3,22 @@ import jsdom from 'jsdom';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Store } from 'redux';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import ConfigMenu, {
-    ConfigMenuProps,
     IConfigMenuProps,
 } from '../../components/configMenu/ConfigMenu';
 import { HelpWith } from '../../components/configMenu/Help';
-import NumberMenuItem, {
-    INumberMenuItemProps,
-} from '../../components/configMenu/menuItems/NumberMenuItem';
 import { initialState } from '../../store/reducers/configReducer';
-import { createStore } from '../../store/store';
 
 let props: IConfigMenuProps;
-let numberProps: INumberMenuItemProps;
-let children: React.ReactNode;
 let window: Window;
-import configureStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
 let map: { [key: string]: any };
 let store: Store;
 
 describe('ConfigMenu', () => {
     beforeEach(() => {
         map = {};
-        numberProps = {
-            name: 'number',
-            configName: 'number',
-            defaultValue: 20,
-            onValidInput: jest.fn(),
-            helpWith: HelpWith.FPS,
-            min: 1,
-            step: 1,
-        };
-        children = <NumberMenuItem {...numberProps} />;
 
         window = new jsdom.JSDOM().window;
         window.addEventListener = jest.fn((event, cb) => {
