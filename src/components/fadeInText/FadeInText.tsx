81117f46e90d7b42cb81545c469400ab1644de09
marcinscottlogic <51320716+marcinscottlogic@users.noreply.github.com>
2019-08-01 11:41:44 +0100

    Feature/281 prompt interactions (#351)
    * implementing fading text
    
    * working on text fade in
    
    * added timeouts for when the person is detected and remove it when noone is in frame
    
    * moved const values to app constants and added clearing timeout when animation is performed
    
    * updated FadeInText component
    
    * removed unused dependency
    
    * rebased on master
    
    * fixed open coefficient issue
    
    * :bug: fixed the import issues
    
    * :truck: Renamed fadeIntext folder to fadeInText
    
     #281
    
    * :label: Updated types for renderToSpan function
    
     #281
    
    * :art: Removed the delays array and generate values inside the function
    
     #351

---
+++ src/components/fadeInText/FadeInText.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { fadeInText } from '../../AppConstants';
+import { normalise } from '../../utils/objectTracking/calculateFocus';
+import './fadeInText.css';
+
+interface ITextProps {
+    text: string;
+    show: boolean;
+}
+
+const FadeInText = React.memo((props: ITextProps) => {
+    function renderSpans(text: string) {
+        const textArray = text.split('');
+
+        const combineWithDelays = (character: string) => ({
+            character,
+            delay: normalise(
+                Math.random(),
+                1,
+                0,
+                fadeInText.delayMax,
+                fadeInText.delayMin,
+            ),
+        });
+
+        return textArray.map(combineWithDelays).map(renderToSpan);
+    }
+
+    function renderToSpan(
+        characterProps: { character: string; delay: number },
+        index: number,
+    ) {
+        const transitionTime = normalise(
+            Math.random(),
+            1,
+            0,
+            fadeInText.transitionMax,
+            fadeInText.transitionMin,
+        );
+        const style = {
+            opacity: props.show ? 1 : 0,
+            transition: `opacity ${transitionTime}ms`,
+            transitionDelay: `${characterProps.delay}ms`,
+            transitionTimingFunction: 'linear',
+        };
+        return (
+            <span style={style} key={index}>
+                {characterProps.character}
+            </span>
+        );
+    }
+
+    return <div className="revealText">{renderSpans(props.text)}</div>;
+});
+
+export default FadeInText;
