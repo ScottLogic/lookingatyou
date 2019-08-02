f7adb73af42b02b4d0439f327f8f055da71d5358
Adam McDevitt <amcdevitt@scottlogic.com>
2019-08-02 13:40:15 +0100

    :recycle: added testing utils file
    added utils file for generating stuff we might want to use during testing. moved getImageData function from select.test.ts to this folder

--- src/__tests__/selectors/detectionSelectors.test.ts
+++ src/__tests__/selectors/detectionSelectors.test.ts
@@ -1,3 +1,8 @@
 import { getSelections } from '../../store/selectors/detectionSelectors';
 
-describe('getSelections', () => {});
+describe('getSelections', () => {
+    const detections = [];
+    const previousTargets = { x: 0, y: 0 };
+    const previousColours = { r: 0, g: 0, b: 0 };
+    const imageData = new ImageData(10, 10);
+});
--- src/__tests__/utils/select/select.test.ts
+++ src/__tests__/utils/select/select.test.ts
@@ -1,4 +1,5 @@
 import { Keypoint } from '@tensorflow-models/posenet';
+import { getImageData } from '../../../__test_utils__/getImageData';
 import { IDetection } from '../../../models/objectDetection';
 import {
     calculateColourMatch,
@@ -74,19 +75,6 @@ const furtherAwayFromPredictionDetection: IDetection = {
 };
 
 const imageData = getImageData(20, 20);
-
-function getImageData(width: number, height: number): ImageData {
-    const data = new Uint8ClampedArray(width * height * 4);
-
-    for (let i = 0; i < width * height * 4; i += 4) {
-        data[i + 0] = 190; // R value
-        data[i + 1] = 0; // G value
-        data[i + 2] = i < width * height * 2 ? 210 : 0; // B value
-        data[i + 3] = 255; // A value
-    }
-
-    return { data, width, height };
-}
 
 function getPart(score: number, position: ICoords, part: string) {
     return {
