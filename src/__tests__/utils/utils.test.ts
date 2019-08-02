fba13969416aec19eb58c14f1e011fdcbc3d456a
Ali Colver <acolver@scottlogic.com>
2019-08-02 09:43:02 +0100

    :camera_flash: Updating snapshots

--- src/__tests__/utils/utils.test.ts
+++ src/__tests__/utils/utils.test.ts
@@ -14,17 +14,6 @@ describe('getBBox should give bbox or undefined', () => {
     });
 });
 
-describe('getLargerDistance should return the larger of the distances', () => {
-    const old1 = { x: 0, y: 0 };
-    const new1 = { x: 10, y: 0 };
-    const old2 = { x: 10, y: -10 };
-    const new2 = { x: -10, y: 50 };
-    it('should work for all number cases', () => {
-        expect(getLargerDistance(old1, new1)).toEqual(10);
-        expect(getLargerDistance(old2, new2)).toEqual(60);
-    });
-});
-
 describe('getImageData should work even on undefined input', () => {
     it('should be empty', () => {
         expect(getImageDataFromVideo(undefined, document)).toBe(null);
