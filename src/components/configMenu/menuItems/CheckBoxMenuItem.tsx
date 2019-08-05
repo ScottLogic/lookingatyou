58600cfb7c7b1a683a141294723e28f80d2aea92
Ali Colver <acolver@scottlogic.com>
2019-08-05 15:28:01 +0100

    :lipstick: Making a much nicer popup for toggle advanced

--- src/components/configMenu/menuItems/CheckBoxMenuItem.tsx
+++ src/components/configMenu/menuItems/CheckBoxMenuItem.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import Button from '@material-ui/core/Button';
+import React, { useState } from 'react';
 import Popup from 'reactjs-popup';
 import { PartialConfig } from '../../../store/actions/config/types';
 import { HelpWith } from '../Help';
+import './CheckBoxMenuItem.css';
 
 export interface ICheckBoxMenuItemProps {
     window: Window;
@@ -15,34 +17,74 @@ export interface ICheckBoxMenuItemProps {
 
 const CheckBoxMenuItem = React.memo(
     (props: ICheckBoxMenuItemProps) => {
+        const [showModal, setShowModal] = useState(false);
+
         function onChange(event: React.ChangeEvent<HTMLInputElement>) {
-            if (
-                props.alert &&
-                !props.window.confirm(
-                    'WARNING: Changing these settings could result in bad performance of the app',
-                )
-            ) {
-                return;
+            if (!props.checked) {
+                setShowModal(true);
+            } else {
+                props.onInputChange({
+                    [props.configName]: event.target.checked,
+                });
             }
+        }
+
+        function accept(event: React.MouseEvent<HTMLButtonElement>) {
+            setShowModal(false);
             props.onInputChange({
-                [props.configName]: event.target.checked,
+                [props.configName]: true,
             });
         }
+
+        function decline(event: React.MouseEvent<HTMLButtonElement>) {
+            setShowModal(false);
+            props.onInputChange({
+                [props.configName]: false,
+            });
+        }
+
         return (
             <div data-tip={true} data-for={HelpWith[props.helpWith]}>
                 <label>{props.name}</label>
-                <Popup
-                    trigger={
-                        <input
-                            type="checkbox"
-                            checked={props.checked}
-                            onChange={onChange}
-                        />
-                    }
-                    position="right center"
-                >
-                    <div>Popup content here !!</div>
-                </Popup>
+
+                <input
+                    type="checkbox"
+                    checked={props.checked}
+                    onChange={onChange}
+                />
+
+                {props.alert && (
+                    <Popup
+                        open={showModal}
+                        modal={true}
+                        closeOnDocumentClick={false}
+                    >
+                        <>
+                            <h1>Warning!</h1>
+                            <br />
+                            The advanced settings are intended for users with a
+                            technical understanding of the app and changing them
+                            from the defaults could lead to the app becoming
+                            unstable.
+                            <br />
+                            <Button
+                                variant="contained"
+                                className="accept"
+                                color="primary"
+                                onClick={accept}
+                            >
+                                Proceed
+                            </Button>
+                            <Button
+                                variant="contained"
+                                className="decline"
+                                onClick={decline}
+                            >
+                                Cancel
+                            </Button>
+                        </>
+                    </Popup>
+                )}
             </div>
         );
     },
