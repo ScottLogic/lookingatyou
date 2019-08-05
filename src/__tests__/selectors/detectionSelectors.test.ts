22708098298b263d166fdfef23ac22090afce936
Adam McDevitt <amcdevitt@scottlogic.com>
2019-08-05 09:15:45 +0100

    :recycle: simplified colour maths in select.ts

--- src/__tests__/selectors/detectionSelectors.test.ts
+++ src/__tests__/selectors/detectionSelectors.test.ts
@@ -1,6 +1,12 @@
 import { getImageData } from '../../__test_utils__/getImageData';
 import { makeDetection } from '../../__test_utils__/makeDetection';
-import { getSelectionsCombiner, getTargetsCombiner } from '../../store/selectors/detectionSelectors';
+import { bodyParts } from '../../AppConstants';
+import { IDetection } from '../../models/objectDetection';
+import {
+    getColourCombiner,
+    getSelectionsCombiner,
+    getTargetsCombiner,
+} from '../../store/selectors/detectionSelectors';
 
 describe('getSelectionCombiner', () => {
     it('should return undefined when there are no detections', () => {
@@ -31,6 +37,52 @@ describe('getSelectionCombiner', () => {
 });
 describe('getTargetsCombiner', () => {
     it('should return the idle targets when selection and/or video is undefined', () => {
-        expect(getTargetsCombiner(undefined, undefined, {x: 100, y:155})).toStrictEqual({x:100,y:155});
-    })
-})
+        expect(
+            getTargetsCombiner(undefined, undefined, { x: 100, y: 155 }),
+        ).toStrictEqual({ x: 100, y: 155 });
+    });
+    it('should return the selection normalised to the video dimensions, when selection and video are both defined', () => {
+        const selection = makeDetection(150, 250);
+        const dimensions = { width: 300, height: 250 };
+        const idleTargets = { x: 0, y: 0 };
+        expect(
+            getTargetsCombiner(selection, dimensions, idleTargets),
+        ).toStrictEqual({ x: 0, y: 1 });
+    });
+});
+describe('getColourCombiner', () => {
+    it('should return 0 for all rgb values when selection is undefined', () => {
+        expect(getColourCombiner(undefined, getImageData(1, 1))).toStrictEqual({
+            r: 0,
+            b: 0,
+            g: 0,
+        });
+    });
+    it('should correctly return colour based on keypoints of the selection and colour values stored in imageData', () => {
+        const selection: IDetection = {
+            bbox: [10, 10, 0, 0],
+            info: {
+                score: 100,
+                keypoints: [
+                    {
+                        score: 1,
+                        position: { x: 5, y: 5 },
+                        part: bodyParts.LEFT_SHOULDER,
+                    },
+                    {
+                        score: 1,
+                        position: { x: 10, y: 5 },
+                        part: bodyParts.LEFT_SHOULDER,
+                    },
+                ],
+            },
+        };
+        const imageData = getImageData(20, 20);
+        console.log(imageData.data);
+        expect(getColourCombiner(selection, imageData)).toStrictEqual({
+            r: 190,
+            g: 0,
+            b: 210,
+        });
+    });
+});
