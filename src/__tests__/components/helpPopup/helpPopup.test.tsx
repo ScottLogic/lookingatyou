b0b0ff6a6d59ee2ad88588922b958d8624823a7f
Adam McDevitt <amcdevitt@scottlogic.com>
2019-08-06 11:32:18 +0100

    :white_check_mark: wrote snapshot test

---
+++ src/__tests__/components/helpPopup/helpPopup.test.tsx
@@ -0,0 +1,27 @@
+import { shallow } from 'enzyme';
+import jsdom, { DOMWindow } from 'jsdom';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Store } from 'redux';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import HelpPopup from '../../../components/helpPopup/HelpPopup';
+import { initialState } from '../../../store/reducers/configReducer';
+
+describe('HelpPopup', () => {
+    let window: DOMWindow;
+    let store: Store;
+    beforeEach(() => {
+        const mockStore = configureStore([thunk]);
+        store = mockStore(initialState);
+        window = new jsdom.JSDOM().window;
+    });
+    it('should render correctly', () => {
+        const wrapper = shallow(
+            <Provider store={store}>
+                <HelpPopup window={window} />
+            </Provider>,
+        ).debug();
+        expect(wrapper).toMatchSnapshot();
+    });
+});
