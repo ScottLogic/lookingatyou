3329ab243cd99a4b9bfa0a4ae2404e5e14fe9561
Adam McDevitt <amcdevitt@scottlogic.com>
2019-08-12 10:44:29 +0100

    :sparkles: Added check for checking saved config format
    Will now compare the saved config's keys to the keys of the initial config. Ideally I would have liked to check that the saved config is an instance of the IConfigState interface, but interfaces cannot be checked against during run time. #478

--- src/store/store.ts
+++ src/store/store.ts
@@ -6,16 +6,51 @@ import {
 } from 'redux';
 import { load, save } from 'redux-localstorage-simple';
 import thunk from 'redux-thunk';
+import { IConfigState } from './actions/config/types';
+import { initialState } from './reducers/configReducer';
 import reducer, { IRootStore } from './reducers/rootReducer';
 
 const states = ['configStore'];
 const middleware: Middleware[] = [thunk, save({ states })];
 
 export function createStore() {
+    const savedState: object | undefined = load({ states });
+    let savedStateIsValid;
+    if (savedState.hasOwnProperty('configStore')) {
+        const previousConfigState: IConfigState = (savedState as {
+            configStore: IConfigState;
+        }).configStore;
+        savedStateIsValid = hasKeysOfInitialConfigState(previousConfigState);
+    } else {
+        savedStateIsValid = false;
+    }
+
     return createReduxStore(
         reducer,
-        load({ states }),
+        savedStateIsValid ? savedState : undefined,
         applyMiddleware(...middleware),
+    );
+}
+
+export function hasKeysOfInitialConfigState(objToCheck: object) {
+    return hasKeysOf(objToCheck, initialState);
+}
+
+export function hasKeysOf(objToCheck: object, objToMatch: object): boolean {
+    const propsToCheck = Object.keys(objToCheck);
+    const propsToMatch = Object.keys(objToMatch);
+    return (
+        typeof objToCheck === 'string' ||
+        typeof objToMatch === 'string' || // prevents infinite recursion
+        propsToMatch.every(prop => {
+            return (
+                propsToCheck.indexOf(prop) >= 0 &&
+                hasKeysOf(
+                    (objToCheck as { [prop: string]: object })[prop],
+                    (objToMatch as { [prop: string]: object })[prop],
+                )
+            );
+        })
     );
 }
 
