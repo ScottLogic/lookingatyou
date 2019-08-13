0b86f5b943915be3e4caff5f46b12196b3d16fbc
Ali Colver <33573952+alicolver@users.noreply.github.com>
2019-08-13 08:38:18 +0100

    🐛 Bug/473 Pose Detection (#489)
    * :bug: Fixing pose detection
    
    * :white_check_mark: Fixing tests

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,9 @@
-import { getBoundingBox, partIds, Pose } from '@tensorflow-models/posenet';
+import {
+    getBoundingBox,
+    Keypoint,
+    partIds,
+    Pose,
+} from '@tensorflow-models/posenet';
 import { IDetection } from '../models/objectDetection';
 import { Bbox } from './types';
 
@@ -40,4 +45,39 @@ export function getImageDataFromVideo(
         }
     }
     return null;
+}
+
+export function checkAngle(
+    point1: Keypoint,
+    point2: Keypoint,
+    point3: Keypoint,
+    inf: number,
+    sup: number,
+): boolean {
+    const angle = (180 * getAngle(point1, point2, point3)) / Math.PI;
+    return angle >= inf && angle <= sup;
+}
+
+function getAngle(
+    pointA: Keypoint,
+    pointB: Keypoint,
+    pointC: Keypoint,
+): number {
+    const sideC = Math.hypot(
+        pointA.position.x - pointB.position.x,
+        pointA.position.y - pointB.position.y,
+    );
+    const sideA = Math.hypot(
+        pointB.position.x - pointC.position.x,
+        pointB.position.y - pointC.position.y,
+    );
+    const sideB = Math.hypot(
+        pointC.position.x - pointA.position.x,
+        pointC.position.y - pointA.position.y,
+    );
+
+    return Math.acos(
+        (Math.pow(sideC, 2) + Math.pow(sideA, 2) - Math.pow(sideB, 2)) /
+            (2 * sideC * sideA),
+    );
 }
