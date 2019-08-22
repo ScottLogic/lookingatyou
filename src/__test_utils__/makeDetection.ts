2045d960722a485f28454b492246a3b35986bcd2
Marcin Szczot <mszczot@scottlogic.com>
2019-08-22 13:41:47 +0100

    :rocket: Demo branch!!!!! :sparkles:

--- src/__test_utils__/makeDetection.ts
+++
@@ -1,5 +0,0 @@
-import { IDetection } from '../models/objectDetection';
-
-export function makeDetection(x: number, y: number): IDetection {
-    return { bbox: [x, y, 0, 0], info: { keypoints: [], score: 0.5 } };
-}
